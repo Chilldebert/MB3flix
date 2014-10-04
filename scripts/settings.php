e659e22e099347b6218c626bd6cd0f7931b58367
Chilldebert <scherzinger.david@web.de>
2014-10-04 12:36:03 +0200

    settings altered

--- scripts/settings.php
+++ scripts/settings.php
@@ -2,13 +2,13 @@
 
 // MediaBrowser Connection Information.
 // Note: We are not doing a complete authentication protocol. 
-$serverIP   = "localhost";
+$serverIP   = "";
 $serverPort = "8096";
-$userHash   = "3f79fc1c4eceb393a69a06a1b739454c";
+$userHash   = "";
 
 // Database Information
 $hostname   = "localhost";
-$username   = "root";
-$password   = "7799225";
-$dbname     = "badger-movies";
+$username   = "";
+$password   = "";
+$dbname     = "";
 $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
